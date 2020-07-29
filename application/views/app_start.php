62d7603dfd21056e3bcd8a9a0955cf55c4200436
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-29 17:50:57 -0500

    Update app_start.php

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -420,12 +420,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 <!-- jQuery first, then Metro UI JS -->
-<script src="<?php echo base_url()."_include/"; ?>vendors/jquery/jquery-3.4.1.min.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>vendors/chartjs/Chart.bundle.min.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>vendors/qrcode/qrcode.min.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>vendors/jsbarcode/JsBarcode.all.min.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>vendors/ckeditor/ckeditor.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>vendors/metro4/js/metro.min.js"></script>
-<script src="<?php echo base_url()."_include/"; ?>js/index.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/jquery/jquery-3.4.1.min.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/chartjs/Chart.bundle.min.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/qrcode/qrcode.min.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/jsbarcode/JsBarcode.all.min.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/ckeditor/ckeditor.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>vendors/metro4/js/metro.min.js"></script>
+<script src="<?php echo APPPATH."views/_include/"; ?>js/index.js"></script>
 </body>
 </html>

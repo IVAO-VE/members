08c3cd3dd7545322ca92b2ff4b03a4a151e03c16
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-29 16:41:10 -0500

    init test

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://members.ve.ivao.aero/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = 'app_start.php';
 
 /*
 |--------------------------------------------------------------------------

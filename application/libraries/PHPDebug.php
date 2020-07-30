4daf0d28dc25816eb9d2c20c333a3ffed165abbf
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-30 18:41:48 -0500

    UTF-8 in files

--- application/libraries/PHPDebug.php
+++ application/libraries/PHPDebug.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @autor Rixio Iguarán y Simón Cardona.
+ * @autor Rixio IguarÃ¡n y SimÃ³n Cardona.
  * @Departamento Sistemas y Webmaster
  * @Licencia Exclusivo sistemas IVAO.AERO
- * @Licencia División Venezuela.
+ * @Licencia DivisiÃ³n Venezuela.
  * @Correo ve-web@ivao.aero
  * 
  **/
 
     //Asegurando el acceso directo al script
-    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
+    defined('BASEPATH') OR exit('El acceso directo al cÃ³digo no estÃ¡ permitido.');
 
 class PHPDebug {
     /** ***************************************************************************************************************************** **/     

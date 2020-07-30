90ec29c23e12085a907b2bb3cf8ec234b4489099
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-30 15:01:40 -0500

    Update app_start.php

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -1,7 +1,7 @@
 <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
     if(!$this->session->userdata('SES-VID')){
-        echo APPPATH;        
+                
     }
     $this->load->view("_lib/lib.header.php");
     $this->load->view("_lib/lib.menu.php");

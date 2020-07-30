d94e6a5a8bbf35961ed5690dacda8ded36ff77ef
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-30 18:41:01 -0500

    Update app_start.php

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -12,7 +12,7 @@
     defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
     //Validando el acceso en IVAO
     if(!$this->session->userdata('SES-VID')){
-        $this->myfunctions->valida_API($_SERVER['SCRIPT_NAME']);
+        //$this->myfunctions->valida_API($_SERVER['SCRIPT_NAME']);
     }
     //Cargando la estructura del HEADER
     $this->load->view("_lib/lib.header.php");

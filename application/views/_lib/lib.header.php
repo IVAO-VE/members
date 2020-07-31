53adb08f38d01add96b8f80774798e9ee939dd7c
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-31 13:46:45 -0500

    Update lib.header.php

--- application/views/_lib/lib.header.php
+++ application/views/_lib/lib.header.php
@@ -23,7 +23,7 @@
     <link rel="stylesheet" href="<?php echo base_url('_include/vendors/metro4/css/metro-all.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('_include/css/index.css'); ?>">
 
-    <title>&#8482; IVAO Venezuela &#171; <?php echo $lang['membersarea']; ?> &#187;</title>
+    <title>IVAO Venezuela | <?php echo $lang['membersarea']; ?></title>
 
     <script>
         window.on_page_functions = [];

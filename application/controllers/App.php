6b7abf3138edc8f5800a58ace40984bc898cc947
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-31 11:32:12 -0500

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -3,9 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class App extends CI_Controller {
 
-	public function index()
-	{
-
+	public function index() {
         //Validando el acceso en IVAO
         $xMIEMBRO = $this->myfunctions->valida_API();
         if(!empty($xMIEMBRO->vid)){

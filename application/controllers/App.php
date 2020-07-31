9a4cfe41b49b9343de2608b273362f601abf1c2f
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-31 00:51:55 -0500

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -7,8 +7,9 @@ class App extends CI_Controller {
 	{
 
         //Validando el acceso en IVAO
-        list($result, $vid, $firstname, $lastname, $rating, $ratingatc, $ratingpilot, $division, $country, $skype, $hours_atc, $hours_pilot, $staff, $va_staff_ids, $va_staff, $va_staff_icaos, $isNpoMember, $va_member_ids, $hq_pilot) = $this->myfunctions->valida_API();
-        if($vid != ''){
+        //list($result, $vid, $firstname, $lastname, $rating, $ratingatc, $ratingpilot, $division, $country, $skype, $hours_atc, $hours_pilot, $staff, $va_staff_ids, $va_staff, $va_staff_icaos, $isNpoMember, $va_member_ids, $hq_pilot) = $this->myfunctions->valida_API();
+        $xArreglo = $this->myfunctions->valida_API();
+        if($xArreglo[0] != '0'){
             $this->load->helper('url');
             $this->load->view('app_start');
         }

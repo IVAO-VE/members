1616cc84f10275bfd79c93ef018ad31bd5943bf2
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-31 21:36:44 -0500

    lang

--- application/language/english/website_lang.php
+++ application/language/english/website_lang.php
@@ -6,6 +6,9 @@
  * @Licencia División Venezuela.
  * @Correo ve-web@ivao.aero
  * 
+ * NNNOOOOOTTTAAAAAA IMPORTANTE
+ * Es estrictamente mandatorio generar las variables en los mismos numeros de linea para todos los archivos de lenguaje
+ * Es decir; los archivos de lenguajes deben de mantener integridad de valiables por numero de línea. 
  **/
 
     //Asegurando el acceso directo al script

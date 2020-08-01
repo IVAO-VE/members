29050c5ecb278440de39a3dc5417c2d777759b20
Rixio Iguaran <ve-wm@ivao.aero>
2020-07-31 23:41:53 -0500

    Create Model_access.php

---
+++ application/models/Model_access.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @autor Rixio Iguarán y Simón Cardona.
+ * @Departamento Sistemas y Webmaster
+ * @Licencia Exclusivo sistemas IVAO.AERO
+ * @Licencia División Venezuela.
+ * @Correo ve-web@ivao.aero
+ * 
+ * NNNOOOOOTTTAAAAAA IMPORTANTE
+ * Es estrictamente mandatorio generar las variables en los mismos numeros de linea para todos los archivos de lenguaje
+ * Es decir; los archivos de lenguajes deben de mantener integridad de valiables por numero de línea. 
+ **/
+
+    //Asegurando el acceso directo al script
+    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
+
+class Model_access extends CI_Model {
+
+        public function get_access($MyVID, $MyDEPARTAMENT) {
+            $this->db->where('vid', $MyVID);
+            $this->db->where($MyDEPARTAMENT, 'true');
+            $q = $this->db->get('permisos');
+            
+            if($q->num_rows() > 0){
+                return true;
+            }else{
+                return false;
+            }
+            
+        }
+
+
+}	
+?>

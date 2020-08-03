debe84e84603a9b71e5d09223423ce9a16246396
Rixio Iguaran <ve-wm@ivao.aero>
2020-08-02 19:19:48 -0500

    languajes

---
+++ application/controllers/LanguageSwitcher.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @autor Rixio Iguarán y Simón Cardona.
+ * @Departamento Sistemas y Webmaster
+ * @Licencia Exclusivo sistemas IVAO.AERO
+ * @Licencia División Venezuela.
+ * @Correo ve-web@ivao.aero
+ * 
+ **/
+
+    //Asegurando el acceso directo al script
+    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
+
+class LanguageSwitcher extends CI_Controller
+{
+    public function __construct() {
+        parent::__construct();     
+    }
+ 
+    function switchLang($language = "") {
+        
+        $language = ($language != "") ? $language : "spanish";
+        $this->session->set_userdata('site_lang', $language);
+        
+        redirect($_SERVER['HTTP_REFERER']);
+        
+    }
+}
---
+++ application/hooks/LanguageLoader.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @autor Rixio Iguarán y Simón Cardona.
+ * @Departamento Sistemas y Webmaster
+ * @Licencia Exclusivo sistemas IVAO.AERO
+ * @Licencia División Venezuela.
+ * @Correo ve-web@ivao.aero
+ * 
+ **/
+
+    //Asegurando el acceso directo al script
+    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
+
+class LanguageLoader
+{
+    function initialize() {
+        $ci =& get_instance();
+        $ci->load->helper('language');
+        $siteLang = $ci->session->userdata('site_lang');
+        if ($siteLang) {
+            $ci->lang->load('message',$siteLang);
+        } else {
+            $ci->lang->load('message','english');
+        }
+    }
+}

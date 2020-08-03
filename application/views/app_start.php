1d89880134be27da6d0b3c423496a9cb2e09f71a
Rixio Iguaran <ve-wm@ivao.aero>
2020-08-03 17:04:45 -0500

    Update app_start.php

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -37,7 +37,7 @@
             <div class="icon bg-cyan fg-white"><span class="mif-local-airport"></span></div>
             <div class="content p-4">
                 <div class="text-upper"><?php echo $this->lang->line('main_tflight'); ?></div>
-                <div class="text-upper text-bold text-lead">90%</div>
+                <div class="text-upper text-bold text-lead"><?php echo $this->myfunctions->segundos_a_horas($this->session->userdata('hours_pilot')); ?></div>
             </div>
         </div>
     </div>

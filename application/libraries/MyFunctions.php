d01b7be60b64f9f8b411760076de94ba3d85df80
SimonCCG <simoncardona01@gmail.com>
2020-08-04 22:43:42 -0400

    Funciones

--- application/libraries/MyFunctions.php
+++ application/libraries/MyFunctions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @autor Rixio Iguarán y Simón Cardona.
  * @Departamento Sistemas y Webmaster
@@ -8,19 +9,21 @@
  * 
  **/
 
-    //Asegurando el acceso directo al script
-    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
+//Asegurando el acceso directo al script
+defined('BASEPATH') or exit('El acceso directo al código no está permitido.');
 
-class MyFunctions {
+class MyFunctions
+{
 
-    /** ***************************************************************************************************************************** **/    
-    function __construct() {
-        
+    /** ***************************************************************************************************************************** **/
+    function __construct()
+    {
     }
-    /** ***************************************************************************************************************************** **/    
-    public function auditar($texto){
+    /** ***************************************************************************************************************************** **/
+    public function auditar($texto)
+    {
         $strLOG = '';
-        $filename = $_SERVER['DOCUMENT_ROOT']."/logs/auditoria [".date('d.m.Y')."].log";
+        $filename = $_SERVER['DOCUMENT_ROOT'] . "/logs/auditoria [" . date('d.m.Y') . "].log";
         //$dataFile = fopen($_SERVER['DOCUMENT_ROOT']."/logs/".$filename, "a+");
         $dia = date("d");
         $mes = date("m");
@@ -28,89 +31,97 @@ class MyFunctions {
         $hora = date("H");
         $minuto = date("i");
         $segundo = date("s");
-        $tiempo = "\n".$dia."/".$mes."/".$anno." ".$hora.":".$minuto.":".$segundo;
-        $strLOG =   str_pad($tiempo, 19, " ", STR_PAD_BOTH)." | ".
-                    str_pad($this->get_cliente_ip(), 15, " ", STR_PAD_BOTH)." | ".
-                    str_pad(basename($_SERVER['PHP_SELF'], ".php"), 17, " ", STR_PAD_RIGHT)." | ".
-                    utf8_decode($texto);
-    
-        if (!write_file($filename, $strLOG)){
-                echo 'Imposible auditar';
+        $tiempo = "\n" . $dia . "/" . $mes . "/" . $anno . " " . $hora . ":" . $minuto . ":" . $segundo;
+        $strLOG =   str_pad($tiempo, 19, " ", STR_PAD_BOTH) . " | " .
+            str_pad($this->get_cliente_ip(), 15, " ", STR_PAD_BOTH) . " | " .
+            str_pad(basename($_SERVER['PHP_SELF'], ".php"), 17, " ", STR_PAD_RIGHT) . " | " .
+            utf8_decode($texto);
+
+        if (!write_file($filename, $strLOG)) {
+            echo 'Imposible auditar';
         }
     }
     /** ***************************************************************************************************************************** **/
-    public function get_HOSTPROTOCOL(){
+    public function get_HOSTPROTOCOL()
+    {
         $MyHOST     = $_SERVER['HTTP_HOST'];
         $MyPROTOCOL = $_SERVER['PROTOCOL'] = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) ? 'https' : 'http';
-        return "$MyPROTOCOL://$MyHOST";  
+        return "$MyPROTOCOL://$MyHOST";
     }
     /** ***************************************************************************************************************************** **/
-    public function get_cliente_ip() {
+    public function get_cliente_ip()
+    {
         $ipaddress = '';
         if (getenv('HTTP_CLIENT_IP'))
             $ipaddress = getenv('HTTP_CLIENT_IP');
-        else if(getenv('HTTP_X_FORWARDED_FOR'))
+        else if (getenv('HTTP_X_FORWARDED_FOR'))
             $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
-        else if(getenv('HTTP_X_FORWARDED'))
+        else if (getenv('HTTP_X_FORWARDED'))
             $ipaddress = getenv('HTTP_X_FORWARDED');
-        else if(getenv('HTTP_FORWARDED_FOR'))
+        else if (getenv('HTTP_FORWARDED_FOR'))
             $ipaddress = getenv('HTTP_FORWARDED_FOR');
-        else if(getenv('HTTP_FORWARDED'))
-           $ipaddress = getenv('HTTP_FORWARDED');
-        else if(getenv('REMOTE_ADDR'))
+        else if (getenv('HTTP_FORWARDED'))
+            $ipaddress = getenv('HTTP_FORWARDED');
+        else if (getenv('REMOTE_ADDR'))
             $ipaddress = getenv('REMOTE_ADDR');
         else
             $ipaddress = 'UNKNOWN';
         return $ipaddress;
-    }    
+    }
     /** ***************************************************************************************************************************** **/
-    public function valida_API($url_GOTO = null){
-        if(isset($_GET['IVAOTOKEN'])) {
-            if($_GET['IVAOTOKEN'] !== 'error') {
-                $cookie= array(
+    public function valida_API($url_GOTO = null)
+    {
+        if (isset($_GET['IVAOTOKEN'])) {
+            if ($_GET['IVAOTOKEN'] !== 'error') {
+                $cookie = array(
                     'name'   => 'ivao_token',
                     'value'  => $_GET['IVAOTOKEN'],
-                    'expire' => time()+3600,
-                );                
-        	   set_cookie($cookie);
-               header('Location: http://members.ve.ivao.aero/');            
-        	   exit;
-            }else{
+                    'expire' => time() + 3600,
+                );
+                set_cookie($cookie);
+                header('Location: http://members.ve.ivao.aero/');
+                exit;
+            } else {
                 echo 'This domain is not allowed to use the Login API! Contact the System Adminstrator!';
                 exit;
             }
         }
-        
+
         $MyCOOKIE = get_cookie('ivao_token');
-        if(!$MyCOOKIE == null) {
-        	$user_array = json_decode(file_get_contents('http://login.ivao.aero/api.php?type=json&token='.$_COOKIE['ivao_token']));
-        	if($user_array->result) {
+        if (!$MyCOOKIE == null) {
+            $user_array = json_decode(file_get_contents('http://login.ivao.aero/api.php?type=json&token=' . $_COOKIE['ivao_token']));
+            if ($user_array->result) {
                 //print_r($user_array);
                 return $user_array;
                 exit;
-        	}else{
-            	set_cookie('ivao_token', '', time()-3600);
+            } else {
+                //set_cookie('ivao_token', '', time()-3600);
                 //delete_cookie('ivao_token');
+                unset($_COOKIE['__cfduid']);
                 unset($_COOKIE['ivao_token']);
-            	header('Location: http://login.ivao.aero/index.php?url=http://members.ve.ivao.aero/');
-            	exit;
+                unset($_COOKIE['ci_session']);
+                header('Location: http://login.ivao.aero/index.php?url=http://members.ve.ivao.aero/');
+                exit;
             }
-        }else{
-        	set_cookie('ivao_token', '', time()-3600);
+        } else {
+            //set_cookie('ivao_token', '', time()-3600);
             //delete_cookie('ivao_token');
+            unset($_COOKIE['__cfduid']);
             unset($_COOKIE['ivao_token']);
-        	header('Location: http://login.ivao.aero/index.php?url=http://members.ve.ivao.aero/');
-        	exit;
-        }        
+            unset($_COOKIE['ci_session']);
+            header('Location: http://login.ivao.aero/index.php?url=http://members.ve.ivao.aero/');
+            exit;
+        }
     }
     /** ***************************************************************************************************************************** **/
-    public function segundos_a_horas($PARAM_segundos) {
+    public function segundos_a_horas($PARAM_segundos)
+    {
         $horas = floor($PARAM_segundos / 3600);
         $minutos = floor(($PARAM_segundos - ($horas * 3600)) / 60);
         $segundos = $PARAM_segundos - ($horas * 3600) - ($minutos * 60);
-    
+
         return $horas . ':' . $minutos . ":" . $segundos;
-    }    
+    }
     /** ***************************************************************************************************************************** **/
     /** ***************************************************************************************************************************** **/
     /** ***************************************************************************************************************************** **/
@@ -132,5 +143,3 @@ class MyFunctions {
     /** ***************************************************************************************************************************** **/
     /** ***************************************************************************************************************************** **/
 }
-
-?>

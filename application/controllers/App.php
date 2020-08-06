39a1066819bf559a5f55548c47ee69747f5632ab
Rixio Iguaran <ve-wm@ivao.aero>
2020-08-06 14:35:41 -0500

    Update App.php

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -234,7 +234,7 @@ class App extends CI_Controller
             if($this->session->userdata('vid') != ""){
                 $this->load->view('app_profile');
             }else{
-                reditect(base_url());
+                redirect(base_url());
             }
         }
 

d9f946d79e1be61495956d43264da671d208178d
Rixio Iguaran <ve-wm@ivao.aero>
2020-08-06 22:31:48 -0500

    Update staff_index.php

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -53,65 +53,6 @@
                 </div>
             </div>
 
-            <div class="tiles-grid tiles-group size-2 fg-white" data-group-title="Images">
-                <div data-role="tile" data-cover="../../images/me.jpg">
-                    <span class="branding-bar">Sergey Pimenov</span>
-                </div>
-                <div data-role="tile" data-effect="animate-fade" data-effect-duration="1000">
-                    <div class="slide" data-cover="../../images/me2.jpg"></div>
-                    <div class="slide" data-cover="../../images/me.jpg"></div>
-                    <div class="slide" data-cover="../../images/me3.jpg"></div>
-                    <span class="branding-bar">Gallery</span>
-                </div>
-                <div data-role="tile" data-size="wide" data-effect="animate-slide-left">
-                    <div class="slide" data-cover="../../images/1.jpg"></div>
-                    <div class="slide" data-cover="../../images/2.jpg"></div>
-                    <div class="slide" data-cover="../../images/3.jpg"></div>
-                    <div class="slide" data-cover="../../images/4.jpg"></div>
-                    <div class="slide" data-cover="../../images/5.jpg"></div>
-                    <span class="branding-bar">Gallery</span>
-                </div>
-                <div data-role="tile" data-size="wide" data-effect="image-set">
-                    <img src="../../images/jeki_chan.jpg">
-                    <img src="../../images/shvarcenegger.jpg">
-                    <img src="../../images/vin_d.jpg">
-                    <img src="../../images/jolie.jpg">
-                    <img src="../../images/jek_vorobey.jpg">
-                </div>
-            </div>
-
-            <div class="tiles-grid tiles-group size-1 fg-white" data-group-title="Office">
-                <div data-role="tile" data-size="small">
-                    <img src="../../images/outlook.png" class="icon">
-                </div>
-                <div data-role="tile" data-size="small" class="bg-brown">
-                    <img src="../../images/word.png" class="icon">
-                </div>
-                <div data-role="tile" data-size="small" class="bg-green">
-                    <img src="../../images/excel.png" class="icon">
-                </div>
-                <div data-role="tile" data-size="small" class="bg-red">
-                    <img src="../../images/access.png" class="icon">
-                </div>
-                <div data-role="tile" data-size="small" class="bg-teal">
-                    <img src="../../images/powerpoint.png" class="icon">
-                </div>
-            </div>
-
-            <div class="tiles-grid tiles-group size-2 fg-white" data-group-title="Games">
-                <div data-role="tile" data-cover="../../images/Battlefield_4_Icon.png">
-                    <span class="branding-bar">Battlefield 4</span>
-                </div>
-                <div data-role="tile" class="bg-green">
-                    <img src="../../images/x-box.png" class="icon">
-                    <span class="branding-bar">XBOX ONE</span>
-                </div>
-                <div data-role="tile" data-size="wide" data-cover="../../images/x-box.png">
-                    <span class="branding-bar">XBOX LIVE</span>
-                    <span class="badge-top">5</span>
-                </div>
-            </div>
-
             <div class="tiles-grid tiles-group size-2 fg-white" data-group-title="General">
                 <a href="https://github.com/olton/Metro-UI-CSS" data-role="tile" class="bg-indigo fg-white">
                     <span class="mif-github icon"></span>

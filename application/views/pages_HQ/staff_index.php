9161d3e165d0e96c2a95d7b66bc932e3c23b0b93
VE-WM <ve-wm@ivao.aero>
2020-08-08 11:07:11 -0500

    .

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -53,38 +53,6 @@
                 </div>
             </div>
 
-            <div class="tiles-grid tiles-group size-2 fg-white" data-group-title="General">
-                <a href="https://github.com/olton/Metro-UI-CSS" data-role="tile" class="bg-indigo fg-white">
-                    <span class="mif-github icon"></span>
-                    <span class="branding-bar">Github</span>
-                    <span class="badge-bottom">30</span>
-                </a>
-                <div data-role="tile" class="bg-cyan fg-white">
-                    <span class="mif-envelop icon"></span>
-                    <span class="branding-bar">Email</span>
-                    <span class="badge-bottom">10</span>
-                </div>
-                <div data-role="tile" class="bg-orange fg-white" data-size="wide">
-                    <span class="mif-chrome icon"></span>
-                    <span class="branding-bar">Chrome</span>
-                </div>
-                <div data-role="tile" data-size="small">
-                    <span class="mif-apple icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-red fg-white">
-                    <span class="mif-bell icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-teal fg-white col-1 row-6">
-                    <span class="mif-windows icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-brown fg-white col-2 row-6">
-                    <span class="mif-wind icon"></span>
-                </div>
-                <div data-role="tile" class="bg-cyan fg-white">
-                    <span class="mif-table icon"></span>
-                    <span class="branding-bar">Tables</span>
-                </div>
-            </div>
         </div>
 </div>
 <?php

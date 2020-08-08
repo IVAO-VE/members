5f802db7a0dcd8f806dda24841b6815fb222166d
VE-WM <ve-wm@ivao.aero>
2020-08-08 11:32:02 -0500

    tabs

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -18,15 +18,17 @@
 ?>
 
 <div class="fg-dark container-fluid start-screen h-100">
-        <h1 class="mb-15 start-screen-title"><?php echo $this->lang->line('staff_dpto01_index'); ?></h1>
+    <h1 class="mb-15 start-screen-title"><?php echo $this->lang->line('staff_dpto01_index'); ?></h1>
 
-        <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
-            <div class="tabs tabs-wrapper vertical left tabs-expand"><div class="expand-title">Home</div><ul data-role="tabs" data-expand="true" data-tabs-position="vertical left" class="w-50 tabs-list" data-role-tabs="true">
-                <li class="active"><a href="#">Home</a></li>
+    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
+        <div class="bg-white p-4 h-100">
+            <ul data-role="tabs" data-expand="true" data-tabs-position="vertical left" class="w-50">
+                <li><a href="#">Home</a></li>
                 <li><a href="#">Profile</a></li>
                 <li><a href="#">Links</a></li>
-            </ul><button type="button" class="hamburger menu-down dark"><span class="line"></span><span class="line"></span><span class="line"></span></button></div>
+            </ul>
         </div>
+    </div>
 </div>
 
 <?php

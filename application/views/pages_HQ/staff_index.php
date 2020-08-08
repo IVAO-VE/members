352e6a4c6bd2dde74072aa314fb9d5a14780765e
VE-WM <ve-wm@ivao.aero>
2020-08-08 11:19:16 -0500

    .

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -16,45 +16,17 @@
     //Cargando la estructura del MENU
     $this->load->view("_lib/lib.menu.php");
 ?>
+
 <div class="fg-dark container-fluid start-screen h-100">
         <h1 class="mb-15 start-screen-title"><?php echo $this->lang->line('staff_dpto01_index'); ?></h1>
 
-        <div class="tiles-area clear">
-            <div class="tiles-grid tiles-group size-2 fg-white" data-group-title="General">
-                <a href="https://github.com/olton/Metro-UI-CSS" data-role="tile" class="bg-indigo fg-white">
-                    <span class="mif-github icon"></span>
-                    <span class="branding-bar">Github</span>
-                    <span class="badge-bottom">30</span>
-                </a>
-                <div data-role="tile" class="bg-cyan fg-white">
-                    <span class="mif-envelop icon"></span>
-                    <span class="branding-bar">Email</span>
-                    <span class="badge-bottom">10</span>
-                </div>
-                <div data-role="tile" class="bg-orange fg-white" data-size="wide">
-                    <span class="mif-chrome icon"></span>
-                    <span class="branding-bar">Chrome</span>
-                </div>
-                <div data-role="tile" data-size="small">
-                    <span class="mif-apple icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-red fg-white">
-                    <span class="mif-bell icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-teal fg-white col-1 row-6">
-                    <span class="mif-windows icon"></span>
-                </div>
-                <div data-role="tile" data-size="small" class="bg-brown fg-white col-2 row-6">
-                    <span class="mif-wind icon"></span>
-                </div>
-                <div data-role="tile" class="bg-cyan fg-white">
-                    <span class="mif-table icon"></span>
-                    <span class="branding-bar">Tables</span>
-                </div>
-            </div>
-
+        <div data-role="panel" data-title-caption="Panel title" data-title-icon="<span class='mif-apps'></span>">
+                Decorate each side of the avocado with twenty oz of cracker crumps.
+                When the individual of result loves the angers of the teacher, the history will know lotus.
+                Pathways experiment from friendships like real stars.
         </div>
 </div>
+
 <?php
 	$this->load->view("_lib/lib.footer.php");
 ?>

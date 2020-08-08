8bfc88fb244dab770084a83e4e073b2a8bb4b506
VE-WM <ve-wm@ivao.aero>
2020-08-08 14:04:27 -0500

    mt

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -20,30 +20,20 @@
 <div class="fg-dark container-fluid start-screen h-100">
     
     <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
-        <div class="bg-white h-100">
+        <div class="mt-10 bg-white h-100">
 
-        <ul data-role="tabs" data-expand="true">
-                <li><a href="#profile-about"><?php echo $this->lang->line('about'); ?></a></li>
+            <ul data-role="tabs" data-expand="true">
+                <li><a href="#permisos"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
                 <li><a href="#profile-activity">Activity</a></li>
                 <li><a href="#">Timeline</a></li>
                 <li><a href="#">Projects</a></li>
             </ul>
 
             <div id="user-profile-tabs-content">
-                <div id="profile-about">
+                <div id="permisos">
                     <br>
-                    <div data-role="panel" data-title-caption="##" data-title-icon="<span class='mif-info'>" data-collapsible="true">
-                        <div class="text-bold">dd</div>
+                    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_HQ_0001'); ?>" data-title-icon="<span class='mif-info'>" data-collapsible="true">
 
-                        <div class="text-bold mt-2">Locations</div>
-                        <address>
-                            Khreschatyk str, Suite 1<br>
-                            Kiev, Ukraine 01001<br>
-                            <abbr title="primary phone number">Phone:</abbr> (123) 456-7890
-                        </address>
-
-                        <div class="text-bold mt-2">About Me</div>
-                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget pharetra felis, sed ullamcorper dui. Sed et elementum neque. Vestibulum pellente viverra ultrices. Etiam justo augue, vehicula ac gravida a, interdum sit amet nisl. Integer vitae nisi id nibh dictum mollis in vitae tortor.</div>
                     </div>
                     <br>
                 </div>

85f732b4189097975afe73d96b324037d34398b4
VE-WM <ve-wm@ivao.aero>
2020-08-08 13:31:54 -0500

    tabs

--- application/views/pages_HQ/staff_index.php
+++ application/views/pages_HQ/staff_index.php
@@ -23,10 +23,25 @@
     <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
         <div class="bg-white h-100">
             <ul data-role="tabs" data-expand="true" data-tabs-position="vertical left" class="w-25">
-                <li><a href="#"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
-                <li><a href="#">[ModuloX]</a></li>
-                <li><a href="#">[ModuloX]</a></li>
+                <li><a href="#_0001"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
+                <li><a href="#_0002">[ModuloX]</a></li>
+                <li><a href="#_0003">[ModuloX]</a></li>
             </ul>
+            <div class="border bd-default no-border-top p-2">
+                <div id="_0001">
+                    A falsis, calceus altus racana. Heavens of emptiness will daily know a remarkable doer. When cooking soaked pumpkin seeds, be sure they are room temperature.
+                </div>
+                <div id="_0002">
+                    The shield is a post-apocalyptic nanomachine. Gar, never mark a fish. Instead of decorating sweet milk with avocado, use a handfull tablespoons salsa verde and one container rosemary plastic bag.
+                </div>
+                <div id="_0003">
+                    Cream soup is just not the same without basil and canned ground pork butts. Elogium prareres, tanquam alter species. Engage, extraterrestrial wind!
+                </div>
+                <div id="_0004">
+                    This is content for hidden tab from dropdown.
+                </div>
+            </div>
+
         </div>
     </div>
 </div>

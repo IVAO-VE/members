6927d1bb9ceb80d39e8218f75f08fb2095f06535
SimonCCG <simoncardona01@gmail.com>
2020-08-11 23:59:08 -0400

    Puta ,

--- application/views/pages_EV/calendar.php
+++ application/views/pages_EV/calendar.php
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         left:'prev, next',
         center:'title',
         right:'today'
-    }
+    },
     editable: true,
     selectable: true,
     businessHours: true,

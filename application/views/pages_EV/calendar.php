760f6abf0f67d117598291bead83a9a97f44149f
SimonCCG <simoncardona01@gmail.com>
2020-08-12 00:29:13 -0400

    Ahora si

--- application/views/pages_EV/calendar.php
+++ application/views/pages_EV/calendar.php
@@ -40,7 +40,7 @@ $this->load->view("_lib/lib.menu.php");
             selectable: true,
             businessHours: true,
             dayMaxEvents: true, // allow "more" link when too many events
-            events: "$this->load->view('pages_EV/event')"
+            events: "<?php $this->load->view('pages_EV/event') ?>"
         });
 
         calendar.render();

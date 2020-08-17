fc7f2e83293b5e43847ed53a352e3da0c4130e1c
SimonCCG <simoncardona01@gmail.com>
2020-08-17 15:41:25 -0400

    Agregado ID al controller

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -65,6 +65,7 @@ class Staff extends CI_Controller
                 $data['result'] = $this->db->get('events')->result();
 
                 foreach ($data['result'] as $key => $value) {
+                    $data['date'][$key]['eid'] = $value->eid;
                     $data['data'][$key]['title'] = $value->title;
                     $data['data'][$key]['start'] = $value->start;
                     $data['data'][$key]['end'] = $value->end;

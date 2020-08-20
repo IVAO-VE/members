c308bcf13c0ff043868990a33fd4e364ce846f3b
SimonCCG <simoncardona01@gmail.com>
2020-08-19 23:29:01 -0400

    Form

--- application/views/pages_EV/staff_calendar.php
+++ application/views/pages_EV/staff_calendar.php
@@ -167,10 +167,16 @@ $this->load->view("_lib/lib.menu.php");
         </div>
     </div>
     <div class="dialog-actions">
-        <input type="submit" id="btnModificar" value="Modificar" class="button primary">
-        <?php echo form_close() ?>
-        <a id="btnEliminar" onclick="<?php echo base_url() ?>staff/EVdelete" href="" class="button danger">Eliminar</a>
-        <button class="button js-dialog-close">Cerrar</button>
+        <div class="gird">
+            <div class="row">
+                <input type="submit" id="btnModificar" value="Modificar" class="button primary">
+                <?php echo form_close() ?>
+                <?php echo form_open('staff/testing') ?>
+                <a id="btnEliminar" onclick="<?php echo base_url() ?>staff/EVdelete" href="" class="button danger">Eliminar</a>
+                <?php echo form_close() ?>
+                <button class="button js-dialog-close">Cerrar</button>
+            </div>
+        </div>
     </div>
 </div>
 

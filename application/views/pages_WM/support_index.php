2a564d6817532f3ac904c7d51d046e4ada9ad2e6
VE-WM <ve-wm@ivao.aero>
2020-08-21 22:51:36 -0500

    dir

--- application/views/pages_WM/support_index.php
+++ application/views/pages_WM/support_index.php
@@ -32,7 +32,7 @@
 </div>
 
 <div class="m-3">
-    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('support_title'); ?>" data-collapsible="true" data-title-icon="<span class='mif-help'></span>" class="mt-4">
+    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('support_title').' ->'.dirname(); ?>" data-collapsible="true" data-title-icon="<span class='mif-help'></span>" class="mt-4">
         <div class="row">
             <iframe src="https://support.ve.ivao.aero" style="border:none;"></iframe>
         </div>

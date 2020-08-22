b59ea7cc2f6255410a157b0bd3d7e67d22ef500d
VE-WM <ve-wm@ivao.aero>
2020-08-21 22:33:32 -0500

    try FRAME

--- application/views/pages_WM/support_index.php
+++ application/views/pages_WM/support_index.php
@@ -31,6 +31,73 @@
     </div>
 </div>
 
+<div data-role="panel" data-title-caption="Monthly Recap Report" data-collapsible="true" data-title-icon="<span class='mif-chart-line'></span>" class="mt-4">
+    <div class="row">
+        <div class="cell-md-8 p-10">
+            <h5 class="text-center">Sales: 1 Jan, 2014 - 30 Jul, 2014</h5>
+            <canvas id="dashboardChart1"></canvas>
+        </div>
+        <div class="cell-md-4 p-10">
+            <h5 class="text-center">Goal Completion</h5>
+            <div class="mt-6">
+                <div class="clear">
+                    <div class="place-left">Add Products to Cart</div>
+                    <div class="place-right"><strong>160</strong>/200</div>
+                </div>
+                <div data-role="progress" data-value="35" data-cls-bar="bg-cyan"></div>
+            </div>
+            <div class="mt-6">
+                <div class="clear">
+                    <div class="place-left">Complete Purchase</div>
+                    <div class="place-right"><strong>310</strong>/400</div>
+                </div>
+                <div data-role="progress" data-value="35" data-cls-bar="bg-red"></div>
+            </div>
+            <div class="mt-6">
+                <div class="clear">
+                    <div class="place-left">Visit Premium Page</div>
+                    <div class="place-right"><strong>480</strong>/800</div>
+                </div>
+                <div data-role="progress" data-value="35"></div>
+            </div>
+            <div class="mt-6">
+                <div class="clear">
+                    <div class="place-left">Send Inquiries</div>
+                    <div class="place-right"><strong>250</strong>/500</div>
+                </div>
+                <div data-role="progress" data-value="35" data-cls-bar="bg-orange"></div>
+            </div>
+            <div class="mt-6">
+                <p class="text-small">Cum brodium resistere, omnes spatiies perdere varius, magnum lanistaes.</p>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="row">
+        <div class="cell-lg-3 cell-sm-6 text-center mt-4">
+            <div class="fg-green"><span class="mif-arrow-drop-up"></span>17%</div>
+            <div class="text-bold">$35,210.43</div>
+            <div class="text-upper">TOTAL REVENUE</div>
+        </div>
+        <div class="cell-lg-3 cell-sm-6 text-center mt-4">
+            <div class="fg-orange"><span class="">=</span>0</div>
+            <div class="text-bold">$10,390.90</div>
+            <div class="text-upper">TOTAL COST</div>
+        </div>
+        <div class="cell-lg-3 cell-sm-6 text-center mt-4">
+            <div class="fg-green"><span class="mif-arrow-drop-up"></span>20%</div>
+            <div class="text-bold">$24,813.53</div>
+            <div class="text-upper">TOTAL PROFIT</div>
+        </div>
+        <div class="cell-lg-3 cell-sm-6 text-center mt-4">
+            <div class="fg-red"><span class="mif-arrow-drop-down"></span>18%</div>
+            <div class="text-bold">1,200</div>
+            <div class="text-upper">GOAL COMPLETIONS</div>
+        </div>
+    </div>
+</div>
 
 
 <?php

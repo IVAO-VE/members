7068705e0b7a44f6ece951b1bbc6afe53b470548
VE-WM <ve-wm@ivao.aero>
2020-08-23 15:23:49 -0500

    TEST DATABASE

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -75,7 +75,10 @@
     <div class="cell-lg-3 cell-md-6 mt-2">
         <div class="more-info-box bg-cyan fg-white">
             <div class="content">
-                <h2 class="text-bold mb-0">00</h2>
+                <?php
+                    $query = $this->db->query("SELECT * FROM whazzup_log WHERE client_type='PILOT' AND vid=".$this->session->userdata('vid'));
+                    echo '<h2 class="text-bold mb-0">'.$query->num_fields().'</h2>';
+                ?>            
                 <div><?php echo $this->lang->line('main_activityFL'); ?></div>
             </div>
             <div class="icon">

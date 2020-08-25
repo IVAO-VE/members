a060592f55ab568f85da7cdeb72b776634e3927c
VE-WM <ve-wm@ivao.aero>
2020-08-25 10:23:04 -0500

    https

--- application/views/pages_FO/information_index.php
+++ application/views/pages_FO/information_index.php
@@ -35,7 +35,7 @@
 <div class="m-3">
     <div data-role="panel" data-title-caption="<?php echo $this->lang->line('information_title'); ?>" data-collapsible="true" data-title-icon="<span class='mif-help'></span>" class="mt-4">
         <div class="row">
-            <iframe src="http://www.inac.gob.ve/eaip/history-local.html" style="border:none;" title="iframe_INAC"></iframe>
+            <iframe src="www.inac.gob.ve/eaip/history-local.html" style="border:none;" title="iframe_INAC"></iframe>
         </div>
     </div>
 </div>

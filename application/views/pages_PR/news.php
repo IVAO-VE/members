8bc89f25a4ccd8b2192269febcdad613de023a69
SimonCCG <simoncardona01@gmail.com>
2020-08-27 14:01:18 -0400

    News

--- application/views/pages_PR/news.php
+++ application/views/pages_PR/news.php
@@ -100,7 +100,7 @@ $this->load->view("_lib/lib.menu.php");
             if ($New != false) : ?>
                 <div class="row">
                     <div class="cell">
-                        <?php echo $stock->id; ?>
+                        <?php echo $New->id; ?>
                     </div>
                 </div>
             <?php else : ?>

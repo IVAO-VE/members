2d6e75f619f20dae29a2df9ce5db2fbba12f0594
SimonCCG <simoncardona01@gmail.com>
2020-09-02 15:17:36 -0400

    Iniciando reorganizacion de PR

--- application/views/pages_PR/staff_news.php
+++ application/views/pages_PR/staff_news.php
@@ -16,14 +16,15 @@ defined('BASEPATH') or exit('El acceso directo al código no está permitido.');
 $this->load->view("_lib/lib.header.php");
 //Cargando la estructura del MENU
 $this->load->view("_lib/lib.menu.php");
-function GetName($vid){
-    if($vid){
-            $this->db->where('vid', $vid);
-            $this->db->select('vid, name');
-            $query = $this->db->get('members_data'); 
+function GetName($vid)
+{
+    if ($vid) {
+        $this->db->where('vid', $vid);
+        $this->db->select('vid, name');
+        $query = $this->db->get('members_data');
 
-            $Name = $query->result_array()[0]['name'];
-            echo $Name;
+        $Name = $query->result_array()[0]['name'];
+        echo $Name;
     }
 }
 ?>
@@ -42,25 +43,64 @@ function GetName($vid){
     </div>
 </div>
 <div class="bg-white p-4">
-<div class="">
-    <?php if ($this->session->flashdata('info')) : ?>
-        <div class="remark primary">
-            <?php echo $this->session->flashdata('info'); ?>
-        </div>
-    <?php endif; ?>
-    <?php if ($this->session->flashdata('error')) : ?>
-        <div class="remark primary">
-            <?php echo $this->session->flashdata('error'); ?>
-        </div>
-    <?php endif; ?>
+    <div class="">
+        <?php if ($this->session->flashdata('info')) : ?>
+            <div class="remark primary">
+                <?php echo $this->session->flashdata('info'); ?>
+            </div>
+        <?php endif; ?>
+        <?php if ($this->session->flashdata('error')) : ?>
+            <div class="remark primary">
+                <?php echo $this->session->flashdata('error'); ?>
+            </div>
+        <?php endif; ?>
 
-    <div class="bg-white p-6 text-center text-leader">
-        Panel de noticias
-        <p class="text-leader2">
-            En este panel podras ver las noticias creadas y publicadas.
-        </p>
-    </div>
-    <br>
+        <div class="fg-dark container-fluid start-screen h-100">
+            <div class="mb-15"></div>
+            <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
+                <div class="bg-white h-100">
+
+                    <ul data-role="tabs" data-expand="true">
+                        <li><a href="#permisos"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
+                        <li><a href="#profile-activity">Activity</a></li>
+                        <li><a href="#">Timeline</a></li>
+                        <li><a href="#">Projects</a></li>
+                    </ul>
+
+                    <div id="user-profile-tabs-content">
+                        <div id="permisos">
+                            <br>
+                            <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_HQ_0001'); ?>" data-title-icon="<span class='mif-info'>" data-collapsible="true">
+
+                            </div>
+                            <br>
+                        </div>
+                        <div id="profile-activity">
+                            <br>
+                            <div data-role="panel" data-title-caption="User activity" data-title-icon="<span class='mif-chart-line'>" data-collapsible="true">
+                                <canvas id="profileChart1"></canvas>
+                            </div>
+                            <br>
+                            <div data-role="panel" data-title-caption="Clients" data-title-icon="<span class='mif-users'>" data-collapsible="true">
+                                <table class="table striped table-border mt-4" data-role="table" data-cls-table-top="row" data-cls-search="cell-md-6" data-cls-rows-count="cell-md-6" data-rows="5" data-rows-steps="5, 10" data-show-activity="false" data-source="<?php echo base_url('_include/'); ?>data/table.json" data-horizontal-scroll="true">
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="bg-white p-6 text-center text-leader">
+            Panel de noticias
+            <p class="text-leader2">
+                En este panel podras ver las noticias creadas y publicadas.
+            </p>
+        </div>
+        <br>
         <table class="table" data-role="table">
             <thead>
                 <tr>

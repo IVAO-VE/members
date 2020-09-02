d1acddeca9274e09d427d1cda5d2ce746d6a05f9
SimonCCG <simoncardona01@gmail.com>
2020-09-02 19:49:47 -0400

    Estado pendiente

--- application/views/pages_TR/staff_index.php
+++ application/views/pages_TR/staff_index.php
@@ -103,7 +103,7 @@ $this->load->view("_lib/lib.menu.php");
 
                                 </td>
                                 <td>
-                                    <a href=""><i class="fa fa-flag-o" aria-hidden="true"></i></a>
+                                    <a href="<?php echo base_url("staff/pendientegca/" . $fila->id  ) ?>"><i class="fa fa-flag-o" aria-hidden="true"></i></a>
                                     <a href=""><i class="fa fa-check" aria-hidden="true"></i></a>
                                     <a href=""><i class="fa fa-ban" aria-hidden="true"></i></a>
                                 </td>

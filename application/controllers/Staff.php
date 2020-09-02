1ff44780bb51a69959b77364c2d27bd437be2cfb
SimonCCG <simoncardona01@gmail.com>
2020-09-02 17:53:53 -0400

    Se nos olvido el controlador

--- application/controllers/Staff.php
+++ application/controllers/Staff.php
@@ -489,4 +489,22 @@ class Staff extends CI_Controller
             }
         }
     }
+
+    public function Training(){
+       //Consultado con la DB
+       $this->phpdebug->debug('[SEGURIDAD] -> Validando niveles de accesos');
+       $query_access  = $this->db->select('*')
+           ->from('permisos')
+           ->where('vid', $this->session->userdata('vid')) //VID de usuario 
+           ->get();
+       $access_nivel = $query_access->row_array();
+       if (!empty($access_nivel)) { //El usuario está registrado en la db de permisos
+           //******************************
+           if ($access_nivel['pages_TR'] != 'true') { //NO TIENE ACCESO A LA ZONA
+               redirect(base_url());
+           } else {
+               $this->load->view("pages_TR/staff_index");
+           }
+       } 
+    }
 }

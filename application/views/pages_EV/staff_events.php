ae2a40f738b6eed487837e7a1a212d0c5416a094
SimonCCG <simoncardona01@gmail.com>
2020-09-02 09:30:38 -0400

    Empezando integracion

--- application/views/pages_EV/staff_events.php
+++ application/views/pages_EV/staff_events.php
@@ -17,7 +17,6 @@ $this->load->view("_lib/lib.header.php");
 //Cargando la estructura del MENU
 $this->load->view("_lib/lib.menu.php");
 ?>
-<div class="bg-white p-4">
 <div class="row border-bottom bd-lightGray m-3">
     <div class="cell-md-4 d-flex flex-align-center">
         <h3 class="dashboard-section-title text-center text-left-md w-100">Administración de eventos</br><small>Versión v2.0</small></h3>
@@ -32,6 +31,45 @@ $this->load->view("_lib/lib.menu.php");
         </ul>
     </div>
 </div>
+<div class="fg-dark container-fluid start-screen h-100">
+    <div class="mb-15"></div>   
+    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
+        <div class="bg-white h-100">
+
+            <ul data-role="tabs" data-expand="true">
+                <li><a href="#permisos"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
+                <li><a href="#profile-activity">Activity</a></li>
+                <li><a href="#">Timeline</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+
+            <div id="user-profile-tabs-content">
+                <div id="permisos">
+                    <br>
+                    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_HQ_0001'); ?>" data-title-icon="<span class='mif-info'>" data-collapsible="true">
+
+                    </div>
+                    <br>
+                </div>
+                <div id="profile-activity">
+                    <br>
+                    <div data-role="panel" data-title-caption="User activity" data-title-icon="<span class='mif-chart-line'>" data-collapsible="true">
+                        <canvas id="profileChart1"></canvas>
+                    </div>
+                    <br>
+                    <div data-role="panel" data-title-caption="Clients" data-title-icon="<span class='mif-users'>" data-collapsible="true">
+                        <table class="table striped table-border mt-4" data-role="table" data-cls-table-top="row" data-cls-search="cell-md-6" data-cls-rows-count="cell-md-6" data-rows="5" data-rows-steps="5, 10" data-show-activity="false" data-source="<?php echo base_url('_include/'); ?>data/table.json" data-horizontal-scroll="true">
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+<div class="bg-white p-4">
 <?php if ($this->session->flashdata('info')) : ?>
     <div class="remark primary">
         <?php echo $this->session->flashdata('info'); ?>

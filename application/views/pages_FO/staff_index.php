504959b0e0664d4c5c5a4e46bf66d6d6ece870d6
VE-WM <ve-wm@ivao.aero>
2020-09-03 11:47:37 -0500

    .

--- application/views/pages_FO/staff_index.php
+++ application/views/pages_FO/staff_index.php
@@ -26,12 +26,50 @@
             <li class="page-item"><a href="/app/index" class="page-link"><span class="mif-meter"></span></a></li>
             <li class="page-item"><a href="/app/profile" class="page-link"><?php echo $this->lang->line('staffarea'); ?></a></li>
             <li class="page-item"><a href="" class="page-link"><?php echo $this->lang->line('dpto02'); ?></a></li>
-            <li class="page-item"><a href="" class="page-link"><?php echo $this->lang->line('staf_option'); ?></a></li>
+            <li class="page-item"><a href="" class="page-link"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
         </ul>
     </div>
 </div>
 
 
+<div class="fg-dark container-fluid start-screen h-100">
+    <div class="mb-15"></div>   
+    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_dpto01_index'); ?>" data-title-icon="<span class='mif-apps'></span>">
+        <div class="bg-white h-100">
+
+            <ul data-role="tabs" data-expand="true">
+                <li><a href="#permisos"><?php echo $this->lang->line('staff_HQ_0001'); ?></a></li>
+                <li><a href="#profile-activity">Activity</a></li>
+                <li><a href="#">Timeline</a></li>
+                <li><a href="#">Projects</a></li>
+            </ul>
+
+            <div id="user-profile-tabs-content">
+                <div id="permisos">
+                    <br>
+                    <div data-role="panel" data-title-caption="<?php echo $this->lang->line('staff_HQ_0001'); ?>" data-title-icon="<span class='mif-info'>" data-collapsible="true">
+
+                    </div>
+                    <br>
+                </div>
+                <div id="profile-activity">
+                    <br>
+                    <div data-role="panel" data-title-caption="User activity" data-title-icon="<span class='mif-chart-line'>" data-collapsible="true">
+                        <canvas id="profileChart1"></canvas>
+                    </div>
+                    <br>
+                    <div data-role="panel" data-title-caption="Clients" data-title-icon="<span class='mif-users'>" data-collapsible="true">
+                        <table class="table striped table-border mt-4" data-role="table" data-cls-table-top="row" data-cls-search="cell-md-6" data-cls-rows-count="cell-md-6" data-rows="5" data-rows-steps="5, 10" data-show-activity="false" data-source="<?php echo base_url('_include/'); ?>data/table.json" data-horizontal-scroll="true">
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
 
 <?php
 	$this->load->view("_lib/lib.footer.php");

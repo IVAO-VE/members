f9a6e04b854c24fbab41c91d1c0b51d5998c983f
VE-WM <ve-wm@ivao.aero>
2020-09-12 14:31:27 -0500

    prueba Valida_API

--- application/libraries/MyFunctions.php
+++ application/libraries/MyFunctions.php
@@ -71,6 +71,11 @@ class MyFunctions
     /** ***************************************************************************************************************************** **/
     public function valida_API($url_GOTO = null)
     {
+        $MyIP = get_cliente_ip();
+        if(($MyIP == "187.189.83.65") || ($MyIP == "0.0.0.0")){ //Maquina Rixio ó Simon
+            exit(); //maquinas del equipo WEBMASTER no validamos API de IVAO
+        }
+        
         if (isset($_GET['IVAOTOKEN'])) {
             if ($_GET['IVAOTOKEN'] !== 'error') {
                 $cookie = array(

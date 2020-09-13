835812a0d601d5b210982cc8338aeb9a66fc75ca
VE-WM <ve-wm@ivao.aero>
2020-09-13 17:21:40 -0500

    close button

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -31,7 +31,7 @@ window.onload = function() {
             "<h3><?php echo $this->session->userdata('fullname'); ?></h3><p>El sistema solicita que confirmes tu correo electrónico oficial, éste paso es necesario para asegurar una buena comunicación</p></br><label>Ingresa tu correo oficial</label><form id='mail_form' action='javascript:' data-role='validator'><input type='text' data-validate='email'><span class='invalid_feedback'>Debes de introducir un correo válido</span><button id='cmdMAIL' class='button mt-2'>Confirmar</button></form>",
             "info",
             {
-                closeButton: false
+                closeButton: true //hay que cambiar a false para que sea mandatorio
             }
         );
     }

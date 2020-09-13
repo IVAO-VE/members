81d2a28262791fc9f77e65b7bb3d281555e8404c
VE-WM <ve-wm@ivao.aero>
2020-09-13 17:18:22 -0500

    varible de sesión mejor

--- application/views/app_start.php
+++ application/views/app_start.php
@@ -28,7 +28,7 @@ window.onload = function() {
     var member_emal = '<?php echo $row_member->mail; ?>';
     if(member_emal == ''){
         Metro.infobox.create(
-            "<h3><?php echo $row_member->name; ?></h3><p>El sistema solicita que confirmes tu correo electrónico oficial, éste paso es necesario para asegurar una buena comunicación</p></br><label>Ingresa tu correo oficial</label><form id='mail_form' action='javascript:' data-role='validator'><input type='text' data-validate='email'><span class='invalid_feedback'>Debes de introducir un correo válido</span><button id='cmdMAIL' class='button mt-2'>Confirmar</button></form>",
+            "<h3><?php echo $this->session->userdata('fullname'); ?></h3><p>El sistema solicita que confirmes tu correo electrónico oficial, éste paso es necesario para asegurar una buena comunicación</p></br><label>Ingresa tu correo oficial</label><form id='mail_form' action='javascript:' data-role='validator'><input type='text' data-validate='email'><span class='invalid_feedback'>Debes de introducir un correo válido</span><button id='cmdMAIL' class='button mt-2'>Confirmar</button></form>",
             "info",
             {
                 closeButton: false

f018faecf62de36f673af7abc26354e37b7023c1
VE-WM <ve-wm@ivao.aero>
2020-09-13 13:46:06 -0500

    orden js metro

--- application/views/_lib/lib.footer.php
+++ application/views/_lib/lib.footer.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * @autor Rixio Iguar�n y Sim�n Cardona.
+ * @autor Rixio Iguarán y Simón Cardona.
  * @Departamento Sistemas y Webmaster
  * @Licencia Exclusivo sistemas IVAO.AERO
- * @Licencia Divisi�n Venezuela.
+ * @Licencia División Venezuela.
  * @Correo ve-web@ivao.aero
  * 
  **/
 
     //Asegurando el acceso directo al script
-    defined('BASEPATH') OR exit('El acceso directo al c�digo no est� permitido.');
+    defined('BASEPATH') OR exit('El acceso directo al código no está permitido.');
 
 ?>
         
@@ -21,12 +21,12 @@
 
 <!-- jQuery first, then Metro UI JS -->
 <script src="<?php echo base_url('_include/vendors/jquery/jquery-3.4.1.min.js'); ?>"></script>
+<script src="<?php echo base_url('_include/vendors/metro4/js/metro.js?ver=@@b-version'); ?>"></script>
 <script src="<?php echo base_url('_include/js/charts.js'); ?>"></script>
 <script src="<?php echo base_url('_include/vendors/chartjs/Chart.bundle.min.js'); ?>"></script>
 <script src="<?php echo base_url('_include/vendors/qrcode/qrcode.min.js'); ?>"></script>
 <script src="<?php echo base_url('_include/vendors/jsbarcode/JsBarcode.all.min.js'); ?>"></script>
 <script src="<?php echo base_url('_include/vendors/ckeditor/ckeditor.js'); ?>"></script>
-<script src="<?php echo base_url('_include/vendors/metro4/js/metro.min.js'); ?>"></script>
 <script src="<?php echo base_url('_include/js/index.js'); ?>"></script>
 <script src="<?php echo base_url('_include/js/start.js'); ?>"></script>
 </body>

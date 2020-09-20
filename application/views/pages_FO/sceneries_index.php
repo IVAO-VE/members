27aff9502aa334c6da3d6ef1ff0601bb2570da09
VE-WM <ve-wm@ivao.aero>
2020-09-20 13:30:04 -0500

    VE-WM, commit automático.

--- application/views/pages_FO/sceneries_index.php
+++ application/views/pages_FO/sceneries_index.php
@@ -35,6 +35,54 @@
 
 <div class="row">
 
+    <?php
+        //Consultando datos de escenarios en el directorio
+        try {
+            $dir = opendir(FCPATH.'uploads/sceneries/'); //creamos el objeto directorio
+            while($elemento = readdir($dir)){ //recorremos todos los elementos del objeto
+                if(($elemento != ".") && ($elemento != "..")){ //no es control de directorios
+                    if(!is_dir(FCPATH.'uploads/sceneries/'.$elemento)){ //es un archivo
+                        $MyFILE_INFO = pathinfo(FCPATH.'uploads/sceneries/'.$elemento);
+                        $MyFILE_INFO = pathinfo(FCPATH.'uploads/sceneries/'.$elemento);
+                        $MyFILE_PART = explode("_", $MyFILE_INFO['filename']);
+                        $MySIM = end($MyFILE_PART);
+                        switch (strtoupper($MySIM)){ 
+                            case "FS2004": //es una carta por instrumentos
+                                $xSIM = "FS2004";
+                            break;
+                            case "FSX": //es una carta visual
+                                $xSIM = "FSX";
+                            break;
+                            case "P3D": //es una carta visual
+                                $xSIM = "Prepar3D";
+                            break;
+                            case "XPLANE": //es una carta visual
+                                $xSIM = "X-Plane";
+                            break;
+                            case "FS2020": //es una carta visual
+                                $xSIM = "FS2020";
+                            break;
+                        }
+                        echo '<tr>
+                                <td>'.$MyFILE_PART[0].'</td>
+                                <td>'.$xSIM.'</td>
+                                <td>'.date('d/m/Y H:i:s', filectime(FCPATH.'uploads/sceneries/'.$elemento)).'</td>
+                                <td>xx</td>
+                              </tr>';
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            //Problema detetado
+            $this->phpdebug->debug('[DEBUG] -> Excepción: '.$e->getMessage());
+            $this->myfunctions->showDIALOG(false, "Control de errores", $e->getMessage(), 5);
+        }                                                    
+    ?>  
+
+
+
+
+
         <div class="cell-lg-4">
             <div class="card image-header">
                 <div class="card-header fg-white" style="background-image: url(<?php echo base_url('_include/images/scenery')."/".rand(0, 30).".jpg"; ?>)">

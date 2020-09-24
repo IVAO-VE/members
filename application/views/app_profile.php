18f5daca393e4b91036991d63ef380192b93d4cf
SimonCCG <simoncardona01@gmail.com>
2020-09-24 16:00:48 -0400

    Test

--- application/views/app_profile.php
+++ application/views/app_profile.php
@@ -193,7 +193,7 @@ $CouCode = strtolower($this->session->userdata('country_code'));
                                             <td><?php echo $FLrul ?></td>
                                             <td><?php echo $flight->fl_departure ?></td>
                                             <td><?php echo $flight->fl_destination ?></td>
-                                            <td><?php echo $this->myfunctions->get_simulator($simulator) ?></td>
+                                            <td><?php echo $simulator ?></td>
                                         </tr>
                                     <?php
                                     }

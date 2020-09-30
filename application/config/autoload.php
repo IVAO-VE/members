774d9bdb1f1faf28e8d1bce3f7440db0133162bf
VE-WM <ve-wm@ivao.aero>
2020-09-30 15:26:10 -0500

    VE-WM, Commit automático

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('PHPDebug', 'MyFunctions', 'session','database');
+$autoload['libraries'] = array('PDFMeta', 'PHPDebug', 'MyFunctions', 'session','database');
 
 /*
 | -------------------------------------------------------------------

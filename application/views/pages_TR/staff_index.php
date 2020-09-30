b174d1b1fa3f9d209ddbf8b99bb4cabedbbfc78c
VE-WM <ve-wm@ivao.aero>
2020-09-30 15:35:59 -0500

    VE-WM, Commit automático

--- application/views/pages_TR/staff_index.php
+++ application/views/pages_TR/staff_index.php
@@ -183,11 +183,11 @@ $this->load->view("_lib/lib.menu.php");
                                                                             $xDOC = "Otras categorías";
                                                                         break;
                                                                     }
-                                                                    $MyPDF = $this->pdfmeta->load(FCPATH.'uploads/documents/'.$elemento);
+                                                                    $this->pdfmeta->load(FCPATH.'uploads/documents/'.$elemento);
                                                                     echo '
                                                                         <tr>
                                                                             <td>'.$xDOC.'</td>
-                                                                            <td>'.$MyPDF->title.'</td>
+                                                                            <td>'.$this->pdfmeta->title.'</td>
                                                                             <td>'.date('d/m/Y H:i:s', filectime(FCPATH.'uploads/documents/'.$elemento)).'</td>
                                                                             <td>xx</td>
                                                                         </tr>

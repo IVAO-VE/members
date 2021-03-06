4bf70779dcdc91d284ecc37805ce0caeb3981059
SimonCCG <simoncardona01@gmail.com>
2021-03-06 13:44:01 -0500

    VE-WM, Commit automático

--- application/views/pages_FO/notams_index.php
+++ application/views/pages_FO/notams_index.php
@@ -34,10 +34,10 @@ $this->load->view("_lib/lib.menu.php");
 <div class="m-3">
     <div class="bg-white text-center">
         <h1>NOTAM <small>Notice To Airmen</small></h1>
+        <p class="text-just">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, nisi tenetur. Harum et impedit asperiores illum ullam rerum atque laboriosam mollitia, omnis inventore totam repellat quisquam dolorem molestias deleniti doloremque!
+        </p>
     </div>
-    <p class="text-just">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, nisi tenetur. Harum et impedit asperiores illum ullam rerum atque laboriosam mollitia, omnis inventore totam repellat quisquam dolorem molestias deleniti doloremque!
-    </p>
     <div data-role="panel" data-title-caption="NOTAMs" data-collapsible="true" data-title-icon="<span class='mif-clipboard'></span>" class="mt-4">
         <div class="row">
             <div class="bg-white p-4">
